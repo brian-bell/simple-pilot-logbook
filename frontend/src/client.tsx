f890ec79c8cb69983cef0aa6439bc04976532cbf
Brian Bell <bellbm@gmail.com>
2026-03-03 21:33:43 -0500

    Add client entry point with React and TanStack Start integration

---
+++ frontend/src/client.tsx
@@ -0,0 +1,14 @@
+import { StrictMode, createElement, startTransition } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { StartClient } from '@tanstack/react-start/client'
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    createElement(
+      StrictMode,
+      null,
+      createElement(StartClient, null),
+    ),
+  )
+})
